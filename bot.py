bf85764f60df3e4465b314f87b4b296162d5b96b
buyyakhoo <45334396+buyyakhoo@users.noreply.github.com>
2020-10-25 23:55:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import random
 import datetime
 from googletrans import Translator
+import time
+
 
 client = commands.Bot(command_prefix = '.')
 
@@ -93,7 +95,7 @@ async def mine(ctx):
 
 #translator
 @client.command(pass_context=True)
-async def translate(ctx, sentence, lang):
+async def tr(ctx, lang, sentence):
     language_1 = {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino'} #filipino
     language_2 = {'fi': 'finnish', 'fr': 'french', 'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer'}  #khmer
     language_3 = {'ko': 'korean', 'ku': 'kurdish (kurmanji)','ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian'}  #romanian
@@ -105,15 +107,17 @@ async def translate(ctx, sentence, lang):
     try:
         channel = ctx.message.channel
         translator = Translator()
-        translated_sentence = translator.translate(sentence, src='en', dest=lang)
+        translated_sentence = translator.translate(sentence, dest=lang)
+        from_lang_src = translated_sentence.src
+        from_lang = from_lang_src.lower()
         embed = discord.Embed(
             title="Translate", 
             description="Translate the absolute life", 
             color=0xff90fd
         )
 
-        embed.add_field(name="english", value=sentence, inline=False)
-        embed.add_field(name=language_try[lang], value=translated_sentence.text, inline=False)
+        embed.add_field(name=language_try[from_lang].capitalize(), value=sentence, inline=False)
+        embed.add_field(name=language_try[lang].capitalize(), value=translated_sentence.text, inline=False)
         embed.set_footer(text="Life Sucks inc.")
         
         await channel.send(embed=embed)
@@ -134,6 +138,40 @@ async def translate(ctx, sentence, lang):
             embed.set_footer(text="Life Sucks inc.")
             await channel.send(embed=embed)
 
+
+#timer
+@client.command(pass_context=True)
+async def timer(ctx, timee):
+    timee = int(timee)
+    while timee >= 1:
+        channel = ctx.message.channel
+        embed = discord.Embed(
+            title="Timer", 
+            description="Timer the absolute life.", 
+            color=0xff4040
+        )
+        embed.add_field(name="Time Left", value=f"{timee} min.", inline=False)
+        embed.set_footer(text="Life Sucks inc.")
+        await channel.send(embed=embed)
+        time.sleep(60)
+        timee -= 1
+        
+    channel = ctx.message.channel
+    authorid = ctx.message.author.id
+    embed = discord.Embed(
+        title="Timer", 
+        description="Timer the absolute life.", 
+        color=0xff4040
+    )
+    embed.add_field(name="Finished!", value=f"<@{authorid}>", inline=False)
+    embed.set_footer(text="Life Sucks inc.")
+    await channel.send(embed=embed)
+'''
+#todo list
+@client.command(pass_context=True)
+async def todo(ctx, todo):
+'''
+
 #help command
 @client.command(pass_context=True)
 async def help(ctx):
@@ -145,13 +183,14 @@ async def help(ctx):
     )
     
     embed.set_author(name="Life Sucks")
-    embed.set_image(url="https://cdn.discordapp.com/attachments/702199048115060876/702199103618285650/imagehelp.jpg")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/702199048115060876/751831552975175710/mei-misaki-another-another-anime-character-wallpaper-preview.jpg")
     embed.add_field(name=".ping", value="show the latency.", inline=False)
     embed.add_field(name=".mine", value="earn LTC to use absolutely nothing", inline=False)
     embed.add_field(name=".date", value="show the date.", inline=False)
-    embed.add_field(name=".translate \"sentence\" [language]", value="translate the absolute life.", inline=False)
+    embed.add_field(name=".tr \"sentence\" [language]", value="translate the absolute life.", inline=False)
+    embed.add_field(name=".timer [min]", value="timer the absolute life.", inline=False)
     embed.set_footer(text="Life Sucks inc.")
-    
     await channel.send(embed=embed)
 
-client.run(INSERT YOUR TOKEN)
+
+client.run('INSERT YOUR TOKEN')
